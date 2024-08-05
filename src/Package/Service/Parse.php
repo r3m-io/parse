5adf5cddbe998161e3c9d02e71dc5f9754a90e16
remco van der Velde <remco.pc@outlook.com>
2024-08-06 00:22:09 +0200

    parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -416,9 +416,22 @@ class Parse
                                 $char = $data[$i];
                                 if(array_key_exists($i - 1, $data)){
                                     $previous = $data[$i - 1];
+                                    if(array_key_exists('execute', $data[$i - 1])){
+                                        $previous = $data[$i - 1]['execute'];
+                                    }
+                                    elseif(array_key_exists('value', $data[$i - 1])){
+                                        $previous = $data[$i - 1]['value'];
+                                    }
+
                                 }
                                 if(array_key_exists($i + 1, $data)){
                                     $next = $data[$i + 1];
+                                    if(array_key_exists('execute', $data[$i + 1])){
+                                        $next = $data[$i - 1]['execute'];
+                                    }
+                                    elseif(array_key_exists('value', $data[$i + 1])){
+                                        $next = $data[$i - 1]['value'];
+                                    }
                                 }
                                 if($char === '\'' && $is_single_quoted === false){
                                     $is_single_quoted = true;
