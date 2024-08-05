3ff11271d8edc12dc6c6e5958900f164199949a2
remco van der Velde <remco.pc@outlook.com>
2024-08-05 16:33:13 +0200

    parse

--- src/Package/Service/Value.php
+++ src/Package/Service/Value.php
@@ -9,11 +9,12 @@ use R3m\Io\Module\File;
 use Exception;
 class Value
 {
-    public static function define(App $object, $input, $flags, $options)
+    public static function define(App $object, $input, $flags, $options): array
     {
         d($input['string']);
         foreach($input['array'] as $nr => $char){
 
         }
+        return $input;
     }
 }
