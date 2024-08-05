39d713b602060c381062aa283c0bea0a8a271d0a
remco van der Velde <remco.pc@outlook.com>
2024-08-05 16:39:38 +0200

    parse

--- src/Package/Service/Value.php
+++ src/Package/Service/Value.php
@@ -13,7 +13,10 @@ class Value
     {
         d($input['string']);
         foreach($input['array'] as $nr => $char){
-            if($input['string'] === ' true + 1 +2'){
+            if($input['string'] === '
+true +
+1 +
+2'){
                 d($char);
             }
         }
