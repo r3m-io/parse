10dcac5b2e3d626cf518f71627a6c79fe7ee39ee
remco van der Velde <remco.pc@outlook.com>
2024-08-05 16:54:52 +0200

    parse

--- src/Package/Service/Value.php
+++ src/Package/Service/Value.php
@@ -12,14 +12,119 @@ class Value
     public static function define(App $object, $input, $flags, $options): array
     {
         d($input['string']);
+        $value = '';
         foreach($input['array'] as $nr => $char){
-            if($input['string'] === '
-true +
-1 +
-2'){
-                d($char);
+            if(
+                in_array(
+                    $char, [
+                        null,
+                        " ",
+                        "\t",
+                        "\n",
+                        "\r"
+                ],
+                    true)
+            ){
+                if($value){
+                    $value = Value::basic($object, $value, $flags, $options);
+                    $value = '';
+
+                }
+                $value = '';
+                continue;
+            }
+            elseif(is_array($char)){
+                $value = '';
+            } else {
+                $value .= $char;
             }
         }
         return $input;
+    }
+
+    public static function basic(App $object, $input, $flags, $options){
+        switch($input){
+            case 'true':
+                return [
+                    'type' => 'boolean',
+                    'value' => $input,
+                    'execute' => true
+                ];
+            case 'false':
+                return [
+                    'type' => 'boolean',
+                    'value' => $input,
+                    'execute' => false
+                ];
+            case 'null':
+                return [
+                    'type' => 'null',
+                    'value' => $input,
+                    'execute' => null
+                ];
+            case '[]':
+                return [
+                    'type' => 'array',
+                    'value' => $input,
+                    'execute' => []
+                ];
+            case '{}':
+                return [
+                    'type' => 'object',
+                    'value' => $input,
+                    'execute' => (object) []
+                ];
+            default:
+                if(is_numeric($input)){
+                    $length = strlen($input);
+                    $data = mb_str_split($input, 1);
+                    $is_float = false;
+                    for($i=0; $i<$length; $i++){
+                        if(
+                            in_array(
+                                $data[$i],
+                                [
+                                    '0',
+                                    '1',
+                                    '2',
+                                    '3',
+                                    '4',
+                                    '5',
+                                    '6',
+                                    '7',
+                                    '8',
+                                    '9',
+                                    ',',
+                                    '_'
+                                ]
+                            )
+                        ){
+                            //nothing
+                        }
+                        elseif($data[$i] === '.'){
+                            $is_float = true;
+                        } else {
+                            return [
+                                'type' => 'string',
+                                'value' => $input,
+                                'execute' => $input
+                            ];
+                        }
+                    }
+                    if($is_float){
+                        return [
+                            'type' => 'float',
+                            'value' => $input,
+                            'execute' => $input + 0
+                        ];
+                    } else {
+                        return [
+                            'type' => 'integer',
+                            'value' => $input,
+                            'execute' => $input + 0
+                        ];
+                    }
+                }
+        }
     }
 }
