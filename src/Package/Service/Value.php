e9d8e5efa3f47b8e79b7f574f96d20524114ac55
remco van der Velde <remco.pc@outlook.com>
2024-08-07 19:21:09 +0200

    parse

--- src/Package/Service/Value.php
+++ src/Package/Service/Value.php
@@ -113,6 +113,7 @@ class Value
                     }
                     elseif(array_key_exists('tag', $char)){
                         if(array_key_exists('modifier', $char)){
+                            d($char);
                             $char = $char['tag'];// . Variable::string_modifier($object, $char['modifier'], $flags, $options);
                         } else {
                             $char = $char['tag'];
