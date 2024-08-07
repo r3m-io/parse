f44f5183f904320c88ac347336ba2f7580a0c1d1
remco van der Velde <remco.pc@outlook.com>
2024-08-07 23:25:16 +0200

    parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -1653,8 +1653,8 @@ class Parse
             $input = $cache->get($hash);
         } else {
             $input = Symbol::define($object, $input, $flags, $options);
-            d($input);
             $input = Cast::define($object, $input, $flags, $options);
+            d($input);
             $input = Method::define($object, $input, $flags, $options);
             $input = Variable::define($object, $input, $flags, $options);
             $input = Value::define($object, $input, $flags, $options);
