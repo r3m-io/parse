5825290490bb66257c35eb4819b4c054a431b028
remco van der Velde <remco.pc@outlook.com>
2024-08-08 18:49:48 +0200

    parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -78,6 +78,7 @@ class Parse
             $tags = Parse::tags($object, $template, $flags, $options);
             $tags = Parse::tags_remove($object, $tags, $flags, $options);
             $tags = Parse::abstract_syntax_tree($object, $tags, $flags, $options);
+            d($tags);
             $is_new = true;
         }
         if(
@@ -1667,7 +1668,6 @@ class Parse
             $input = Variable::define($object, $input, $flags, $options);
             $input = Value::define($object, $input, $flags, $options);
             $input = Value::array($object, $input, $flags, $options);
-            d($input);
 //        d($input['string']);;
             $input = Parse::cleanup($object, $input, $flags, $options);
             $cache->set($hash, $input);
