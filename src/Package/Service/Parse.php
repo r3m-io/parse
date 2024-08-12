1002b9bb49ed14f1dc6168b28d66463bee775254
remco van der Velde <remco.pc@outlook.com>
2024-08-12 18:45:31 +0200

    parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -1663,9 +1663,10 @@ class Parse
             $input = Symbol::define($object, $input, $flags, $options);
             $input = Cast::define($object, $input, $flags, $options);
             $input = Method::define($object, $input, $flags, $options);
-            d($input);
             $input = Variable::define($object, $input, $flags, $options);
+            d($input);
             $input = Variable::modifier($object, $input, $flags, $options);
+            d($input);
 //            $input = Value::define($object, $input, $flags, $options);
             $input = Value::double_quoted_string($object, $input, $flags, $options);
             $input = Value::array($object, $input, $flags, $options);
