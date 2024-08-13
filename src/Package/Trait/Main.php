c4c857e6935006b3675f686ee8c732f24a88a899
remco van der Velde <remco.pc@outlook.com>
2024-08-13 19:57:03 +0200

    parse

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -5,6 +5,7 @@ use R3m\Io\App;
 use R3m\Io\Config;
 
 use Package\R3m\Io\Parse\Service\Parse;
+use Package\R3m\Io\Parse\Service\Token;
 
 use Exception;
 
@@ -15,6 +16,10 @@ trait Main {
      */
     public function compile($flags, $options){
         $object = $this->object();
+        $token = Token::tokenize($object, $flags, $options);
+        ddd($token);
+
+
         Parse::compile($object, $flags, $options);
         d($flags);
         ddd($options);
