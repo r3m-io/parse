89d8d8cdb6cd3dc3cc175a183b8f201ae88823a2
remco van der Velde <remco.pc@outlook.com>
2024-08-13 15:22:36 +0200

    parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -1822,11 +1822,12 @@ class Parse
             $input = Cast::define($object, $input, $flags, $options);
             $input = Method::define($object, $input, $flags, $options);
             $input = Variable::define($object, $input, $flags, $options);
+
+//            d($input);
+            $input = Variable::modifier($object, $input, $flags, $options);
             if(property_exists($options, 'debug')){
                 ddd($input);
             }
-//            d($input);
-            $input = Variable::modifier($object, $input, $flags, $options);
 //            d($input);
 //            $input = Value::define($object, $input, $flags, $options);
             $input = Value::double_quoted_string($object, $input, $flags, $options);
