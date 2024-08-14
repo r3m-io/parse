333e553b3eef6501acbbb12c231bb33cc0e384cc
remco van der Velde <remco.pc@outlook.com>
2024-08-14 18:28:24 +0200

    parse

--- src/Package/Service/Variable.php
+++ src/Package/Service/Variable.php
@@ -20,8 +20,6 @@ class Variable
         $is_variable = false;
         $has_name = false;
         $name = '';
-        trace();
-        d($input['array']);
         foreach($input['array'] as $nr => $char){
             $previous = Token::item($input, $nr - 1);
             $next = Token::item($input, $nr + 1);
