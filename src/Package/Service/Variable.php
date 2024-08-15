736457983d5f0accc21a27afb6e95df40f9ac575
remco van der Velde <remco.pc@outlook.com>
2024-08-16 00:40:51 +0200

    parse

--- src/Package/Service/Variable.php
+++ src/Package/Service/Variable.php
@@ -316,7 +316,7 @@ class Variable
                 $is_double_quote === false &&
                 $is_double_quote_backslash === false &&
                 (
-                    $set_depth === $set_depth_modifier |
+                    $set_depth === $set_depth_modifier ||
                     $set_depth_modifier === false
                 )
             ){
