13a43ab461d88987c4f9a1168315bac97583d067
remco van der Velde <remco.pc@outlook.com>
2024-08-20 14:40:55 +0200

    parse

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -27,7 +27,6 @@ class Build
                         $data[] = $line;
                     }
                 }
-                d($record);
                 $variable_assign = Build::variable_assign($object, $flags, $options, $record);
                 if($variable_assign){
                     $data[] = $variable_assign;
