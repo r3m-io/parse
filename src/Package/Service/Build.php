5661a8c64b903d10eeddabb0ff707a159ac6f5ca
remco van der Velde <remco.pc@outlook.com>
2024-08-20 19:50:40 +0200

    parse

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -524,13 +524,7 @@ class Build
             if(
                 array_key_exists('is_single_quoted', $record) &&
                 array_key_exists('execute', $record) &&
-                $record['is_single_quoted'] === true ||
-                (
-                    array_key_exists('type', $record) &&
-                    array_key_exists('is_raw', $record) &&
-                    $record['type'] === 'string' &&
-                    $record['is_raw'] === true
-                )
+                $record['is_single_quoted'] === true
             ){
                 $value .= $record['execute'];
             }
