c87672a1a3a3741a5b0964cccabd94e714a706b3
remco van der Velde <remco.pc@outlook.com>
2024-09-01 11:58:05 +0200

    parse

--- src/Package/Service/Variable.php
+++ src/Package/Service/Variable.php
@@ -143,11 +143,11 @@ class Variable
         $argument = [];
         $argument_array = [];
         $nr = $count - 1;
+        d($input);
         foreach($input['array'] as $nr => $char) {
             $previous = Token::item($input, $nr - 1);
             $next = Token::item($input, $nr + 1);
             $current = Token::item($input, $nr);
-            d($current);
             if($current === '('){
                 $set_depth++;
                 if(array_key_exists($argument_nr, $argument)){
