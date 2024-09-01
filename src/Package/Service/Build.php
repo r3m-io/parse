c03d23586bd1cb002e70a9482cd833d558eea1c1
remco van der Velde <remco.pc@outlook.com>
2024-09-01 23:36:40 +0200

    parse

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -504,15 +504,7 @@ class Build
         $object->config('package.r3m_io/parse.build.state.indent', $indent);
         d($indent);
         foreach($record['method']['argument'] as $nr => $argument) {
-            $value = Build::value($object, $flags, $options, $argument);
-            $value = str_repeat(' ', $indent * 4) .
-                str_replace(
-                    PHP_EOL,
-                    PHP_EOL .
-                    str_repeat(' ', $indent * 4),
-                    $value
-                );
-            $method_value .= $value  . ',' . PHP_EOL;
+            $method_value = Build::value($object, $flags, $options, $argument)  . ',' . PHP_EOL;
             $is_argument = true;
         }
         if($is_argument){
