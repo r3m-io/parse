8162df37be3403bedf5fe3d57b5a478e7700385b
remco van der Velde <remco.pc@outlook.com>
2024-09-02 20:10:30 +0200

    parse

--- src/Package/Service/Validator.php
+++ src/Package/Service/Validator.php
@@ -10,13 +10,13 @@ class Validator
         // Create a temporary file and write the PHP code into it
         $tempFile = tempnam(sys_get_temp_dir(), 'PHP');
         file_put_contents($tempFile, "<?php\n" . $code . "\n");
-
         // Use PHP's built-in syntax checker
-        $output = shell_exec("php -l " . escapeshellarg($tempFile));
-
+        exec("php -l " . escapeshellarg($tempFile), $output, $code);
         // Delete the temporary file
         unlink($tempFile);
         ob_end_clean();
+        d($output);
+        ddd($code);
         // Check the output to see if any syntax errors were found
         if (strpos($output, 'No syntax errors detected') !== false) {
             return true;
