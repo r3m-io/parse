8716889832e6f72205a0aa32d442f8b2c8823820
remco van der Velde <remco.pc@outlook.com>
2024-09-02 18:37:28 +0200

    parse

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -530,6 +530,7 @@ class Build
         $indent--;
         $data[] = str_repeat(' ', $indent * 4) . '}';
         $object->config('package.r3m_io/parse.build.state.indent', $indent);
+        d($indent);
         return implode(PHP_EOL, $data);
     }
 
