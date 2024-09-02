66001bba6d67847fd95cd5ca2452f061acb9e5a0
remco van der Velde <remco.pc@outlook.com>
2024-09-02 20:02:47 +0200

    parse

---
+++ src/Package/Service/Validator.php
@@ -0,0 +1,26 @@
+<?php
+namespace Package\R3m\Io\Parse\Service;
+
+class Validator
+{
+
+    public static function validate($code): bool | string
+    {
+        // Create a temporary file and write the PHP code into it
+        $tempFile = tempnam(sys_get_temp_dir(), 'PHP');
+        file_put_contents($tempFile, "<?php\n" . $code . "\n");
+
+        // Use PHP's built-in syntax checker
+        $output = shell_exec("php -l " . escapeshellarg($tempFile));
+
+        // Delete the temporary file
+        unlink($tempFile);
+
+        // Check the output to see if any syntax errors were found
+        if (strpos($output, 'No syntax errors detected') !== false) {
+            return true;
+        } else {
+            return $output;
+        }
+    }
+}
