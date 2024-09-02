5645ecf5ec33ea90127ee50d5ef0532f3fceeab6
remco van der Velde <remco.pc@outlook.com>
2024-09-02 21:13:42 +0200

    parse

--- src/Package/Service/Validator.php
+++ src/Package/Service/Validator.php
@@ -1,21 +1,28 @@
 <?php
 namespace Package\R3m\Io\Parse\Service;
 
+use R3m\Io\App;
+
+use R3m\Io\Module\Core;
+
 class Validator
 {
 
-    public static function validate($string, &$code=null): bool | string
+    public static function validate(App $object, $string, &$code=null): bool | string
     {
         ob_start();
         // Create a temporary file and write the PHP code into it
         $tempFile = tempnam(sys_get_temp_dir(), 'PHP');
         file_put_contents($tempFile, "<?php\n" . $string . "\n");
         // Use PHP's built-in syntax checker
-        exec("php -l " . escapeshellarg($tempFile), $output, $code);
+        Core::execute($object, 'php -l ' . escapeshellarg($tempFile), $output, $notification);
+//        exec("php -l " . escapeshellarg($tempFile), $output, $code);
         // Delete the temporary file
         unlink($tempFile);
         ob_end_clean();
-        $output = implode(PHP_EOL, $output);
+        d($notification);
+        ddd($output);
+//        $output = implode(PHP_EOL, $output);
         // Check the output to see if any syntax errors were found
         if (strpos($output, 'No syntax errors detected') !== false) {
             return true;
