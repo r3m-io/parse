6d39de45f4c18effa4581c747403b988a950c67a
remco van der Velde <remco.pc@outlook.com>
2024-09-08 11:10:31 +0200

    format code

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -378,6 +378,7 @@ class Build
                     $is_double_quote === false &&
                     $char !== ' '
                 ){
+                    d($line);
                     $skip_space = 0;
                 }
                 if($variable_assign_next_tag === false){
